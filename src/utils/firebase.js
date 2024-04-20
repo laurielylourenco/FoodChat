541d97f3914ea0fc512080244b4071fe4f020e08
Lauriely Lourenco <laurielylourenco@gmail.com>
2024-04-20 14:41:02 -0300

    feat:  variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,8 +6,8 @@ export const firebaseConfig = {
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID, 
     appId: process.env.NEXT_PUBLIC_IDAPP,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-    hom: process.env.NEXT_PUBLIC_HOM,
-    hom1: process.env.HOM
+    hom: process.env.REACT_APP_VERCEL_ENV,
+    hom1: process.env.REACT_APP_VERCEL_ENV.HOM
 }
 
 
