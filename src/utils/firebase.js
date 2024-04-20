e9e6a9132349ab2532207ce184b1e912b06bbf42
Lauriely Lourenco <laurielylourenco@gmail.com>
2024-04-20 14:47:00 -0300

    feat:  variables env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API,  
-    authDomain: process.env.NEXT_PUBLIC_DOMAIN ,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID, 
-    appId: process.env.NEXT_PUBLIC_IDAPP,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_API,  
+    authDomain: process.env.REACT_APP_DOMAIN ,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, 
+    appId: process.env.REACT_APP_IDAPP,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
     hom: process.env.REACT_APP_VERCEL_ENV,
-    hom1: process.env.REACT_APP_VERCEL_ENV.HOM
+    hom1: process.env.REACT_APP_HOM
 }
 
 
